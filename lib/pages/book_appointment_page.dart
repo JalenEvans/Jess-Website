5621634bf5a14dc22642e4232c984a16c13d84d7
JalenEvans <jalenemmanuelevans@gmail.com>
2025-03-07 23:24:36 -0500

    Added a barebones home_page and AppBar
    home_page has welcome text and AppBar can navigate between home_page,
    book_appointment_page, and pricing_page.

---
+++ lib/pages/book_appointment_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:jess_website/widgets/app_bar.dart';
+
+class BookAppointmentPage extends StatefulWidget {
+  const BookAppointmentPage({super.key});
+
+  @override
+  State<BookAppointmentPage> createState() => _BookAppointmentPageState();
+}
+
+class _BookAppointmentPageState extends State<BookAppointmentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(context, Theme.of(context).colorScheme),
+      body: Center(
+        child: ListView(
+          children: [
+            SizedBox(height: 100),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Book an Appointment',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/pricing');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Theme.of(context).colorScheme.primaryContainer,
+                  ),
+                  child: const Text('Pricing'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
